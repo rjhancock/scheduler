cb23b36c1498a62c319350facb4a7e29fbd48a79
medium0fry <medium0fry@gmail.com>
2021-06-07 12:40:12 -0400

    Creating columns as drop zones and adding tickets to them

--- client/src/components/Column/Column.js
+++ client/src/components/Column/Column.js
@@ -1,8 +1,34 @@
 
-const Column = () => {
-   return (
-      <div>
+import { Droppable } from 'react-beautiful-dnd';
 
+import Ticket from '../Ticket/Ticket';
+
+import './Column.css';
+
+const Column = ({column, tickets}) => {
+   return (
+      <div className="col">
+         <h3>{column.title}</h3>
+         <Droppable droppableId={column.id}>
+            {(provided) => (
+               <div
+                  className="ticket-list"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+               >
+                  {
+                     tickets.map((ticket, index) => (
+                        <Ticket
+                           key={ticket.id}
+                           ticket={ticket}
+                           index={index}
+                        />
+                     ))
+                  }
+                  {provided.placeholder}
+               </div>
+            )}
+         </Droppable>
       </div>
    );
 }
