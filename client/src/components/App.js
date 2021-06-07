0e9aca38c64fefb1ed11c09b3d0f4bcd39a4f8cb
medium0fry <medium0fry@gmail.com>
2021-06-07 14:36:58 -0400

    Added feature to drag and drop between containers. Prettier-ed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,43 +1,72 @@
+import { useState } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
 
-import { useState } from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from "../initial-data";
+import Column from "./Column/Column";
 
-import initialData from '../initial-data';
-import Column from './Column/Column';
-
-import './App.css';
+import "./App.css";
 
 const App = () => {
 	const [state, setState] = useState(initialData);
 
-	const sameDrop = (source, destination) => {
-		return (destination.droppableId === source.droppableId
-			  && destination.index === source.index);
-	}
+	const isSameLocation = (src, dst) =>
+		dst.droppableId === src.droppableId && dst.index === src.index;
 
-	const onDragEnd = result => {
-		const {destination, source, draggableId} = result;
-
-		// No need to update state if nothing changes
-		if (!destination) return;
-		if (sameDrop(source, destination)) return;
-
-		const column = state.columns[source.droppableId];
+	const dropSame = (src, dst, ticket) => {
+		const column = state.columns[src.droppableId];
 
 		const newTicketIds = Array.from(column.ticketIds);
-		newTicketIds.splice(source.index, 1);
-		newTicketIds.splice(destination.index, 0, draggableId);
+		newTicketIds.splice(src.index, 1);
+		newTicketIds.splice(dst.index, 0, ticket);
 
 		const newColumn = {
 			...column,
 			ticketIds: newTicketIds,
-		}
+		};
 
 		const newState = {
 			...state,
 			columns: {
 				...state.columns,
 				[newColumn.id]: newColumn,
+			},
+		};
+
+		setState(newState);
+	};
+
+	const onDragEnd = (result) => {
+		const { destination, source, draggableId } = result;
+
+		// No need to update state if nothing changes
+		if (!destination) return;
+		if (isSameLocation(source, destination)) return;
+
+		const start = state.columns[source.droppableId];
+		const finish = state.columns[destination.droppableId];
+
+		if (start === finish) return dropSame(source, destination, draggableId);
+
+		const startTaskIds = Array.from(start.ticketIds);
+		startTaskIds.splice(source.index, 1);
+		const newStart = {
+			...start,
+			ticketIds: startTaskIds,
+		};
+
+		const finishTaskIds = Array.from(finish.ticketIds);
+		finishTaskIds.splice(destination.index, 0, draggableId);
+		const newFinish = {
+			...finish,
+			ticketIds: finishTaskIds,
+		};
+
+		const newState = {
+			...state,
+			columns: {
+				...state.columns,
+				[newStart.id]: newStart,
+				[newFinish.id]: newFinish,
 			},
 		};
 
@@ -51,23 +80,19 @@ const App = () => {
 			onDragEnd={onDragEnd}
 		>
 			<div className="board">
-				{
-					state.columnOrder.map((cid) => {
-						const column = state.columns[cid];
-						const tickets = column.ticketIds.map(tid => state.tickets[tid]);
+				{state.columnOrder.map((cid) => {
+					const column = state.columns[cid];
+					const tickets = column.ticketIds.map(
+						(tid) => state.tickets[tid]
+					);
 
-						return (
-							<Column
-								key={column.id}
-								column={column}
-								tickets={tickets}
-							/>
-						);
-					})
-				}
+					return (
+						<Column key={column.id} column={column} tickets={tickets} />
+					);
+				})}
 			</div>
 		</DragDropContext>
 	);
-}
+};
 
 export default App;
