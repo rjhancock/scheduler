036ba29b18dda01c49af2b3481f8d0e83e422805
medium0fry <medium0fry@gmail.com>
2021-06-07 12:40:31 -0400

    Creating tickets as draggable items

--- client/src/components/Ticket/Ticket.js
+++ client/src/components/Ticket/Ticket.js
@@ -1,9 +1,21 @@
 
-const Ticket = () => {
-	return (
-		<div>
+import { Draggable } from 'react-beautiful-dnd';
 
-		</div>
+import './Ticket.css'
+
+const Ticket = ({ticket, index}) => {
+	return (
+		<Draggable draggableId={ticket.id} index={index}>
+			{(provided) => (
+				<div className="ticket"
+					{...provided.draggableProps}
+					{...provided.dragHandleProps}
+					ref={provided.innerRef}
+				>
+					{ticket.content}
+				</div>
+			)}
+		</Draggable>
 	);
 }
 
