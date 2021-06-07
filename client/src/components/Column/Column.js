2d6cff833f2f2f9717e91f6426057f124ef6dcbd
medium0fry <medium0fry@gmail.com>
2021-06-07 14:34:13 -0400

    Added color when dropping. Prettier-ed the file

--- client/src/components/Column/Column.js
+++ client/src/components/Column/Column.js
@@ -1,36 +1,31 @@
+import { Droppable } from "react-beautiful-dnd";
 
-import { Droppable } from 'react-beautiful-dnd';
+import Ticket from "../Ticket/Ticket";
 
-import Ticket from '../Ticket/Ticket';
+import "./Column.css";
 
-import './Column.css';
-
-const Column = ({column, tickets}) => {
-   return (
-      <div className="col">
-         <h3>{column.title}</h3>
-         <Droppable droppableId={column.id}>
-            {(provided) => (
-               <div
-                  className="ticket-list"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-               >
-                  {
-                     tickets.map((ticket, index) => (
-                        <Ticket
-                           key={ticket.id}
-                           ticket={ticket}
-                           index={index}
-                        />
-                     ))
-                  }
-                  {provided.placeholder}
-               </div>
-            )}
-         </Droppable>
-      </div>
-   );
-}
+const Column = ({ column, tickets }) => {
+	return (
+		<div className="col">
+			<h3>{column.title}</h3>
+			<Droppable droppableId={column.id}>
+				{(provided, snapshot) => (
+					<div
+						className={`ticket-list${
+							snapshot.draggingOverWith ? " drop" : ""
+						}`}
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+					>
+						{tickets.map((ticket, index) => (
+							<Ticket key={ticket.id} ticket={ticket} index={index} />
+						))}
+						{provided.placeholder}
+					</div>
+				)}
+			</Droppable>
+		</div>
+	);
+};
 
 export default Column;
