9b9f33f32399806d0d450802b1450a90c7413035
medium0fry <medium0fry@gmail.com>
2021-06-07 20:41:05 -0400

    Allowed Columns to be draggable on the board

--- client/src/components/Column/Column.js
+++ client/src/components/Column/Column.js
@@ -1,30 +1,42 @@
-import { Droppable } from "react-beautiful-dnd";
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 
-import Ticket from "../Ticket/Ticket";
+import Ticket from '../Ticket/Ticket';
 
-import "./Column.css";
+import './Column.css';
 
-const Column = ({ column, tickets }) => {
+const Column = ({ column, tickets, index }) => {
 	return (
-		<div className="col">
-			<h3>{column.title}</h3>
-			<Droppable droppableId={column.id}>
-				{(provided, snapshot) => (
-					<div
-						className={`ticket-list${
-							snapshot.draggingOverWith ? " drop" : ""
-						}`}
-						{...provided.droppableProps}
-						ref={provided.innerRef}
-					>
-						{tickets.map((ticket, index) => (
-							<Ticket key={ticket.id} ticket={ticket} index={index} />
-						))}
-						{provided.placeholder}
-					</div>
-				)}
-			</Droppable>
-		</div>
+		<Draggable draggableId={column.id} index={index}>
+			{(provided) => (
+				<div
+					className="col"
+					{...provided.draggableProps}
+					ref={provided.innerRef}
+				>
+					<h3 {...provided.dragHandleProps}>{column.title}</h3>
+					<Droppable droppableId={column.id} type="ticket">
+						{(provided, snapshot) => (
+							<div
+								className={`ticket-list${
+									snapshot.draggingOverWith ? ' drop' : ''
+								}`}
+								{...provided.droppableProps}
+								ref={provided.innerRef}
+							>
+								{tickets.map((ticket, index) => (
+									<Ticket
+										key={ticket.id}
+										ticket={ticket}
+										index={index}
+									/>
+								))}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
+				</div>
+			)}
+		</Draggable>
 	);
 };
 
