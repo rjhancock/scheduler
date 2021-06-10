2bb2f62ab2d98523484fc4dc8d686d5629a574e5
medium0fry <medium0fry@gmail.com>
2021-06-09 21:39:58 -0400

    Wrapping app in MUI Container

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,18 @@
 import Board from './Board/Board';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
 import './App.css';
 
 const App = () => {
 	return (
 		<>
-			<Board />
+			<Container className="flex">
+				<Board />
+				<Button variant="contained" color="primary">
+					Make A Request
+				</Button>
+			</Container>
 		</>
 	);
 };
