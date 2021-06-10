438e135a70b26c21544089e25d4248bcf9304b7c
medium0fry <medium0fry@gmail.com>
2021-06-10 17:02:51 -0400

    Made bulk importing of pages easier and cleaner

---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Artists } from './Artists';
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Request } from './Request';
+export { default as SignUp } from './SignUp';
