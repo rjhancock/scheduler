71a3db9fad4e62803d80c6c58400be242218ba0d
medium0fry <medium0fry@gmail.com>
2021-06-12 20:40:24 -0400

    Filled out Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,73 @@
 import { Link } from 'react-router-dom';
 
-const SignUp = () => {
+import {
+	Button,
+	Container,
+	Grid,
+	InputAdornment,
+	TextField,
+	Typography,
+} from '@material-ui/core';
+
+import PersonIcon from '@material-ui/icons/PersonOutlineOutlined';
+import PasswordIcon from '@material-ui/icons/LockOutlined';
+
+const Login = () => {
 	return (
-		<div style={{ display: 'flex', flexFlow: 'column' }}>
-			<h1>Login Page</h1>
-			<span>
-				Need an account? <Link to="/signup">Sign Up</Link>
-			</span>
-		</div>
+		<Container component="main" maxWidth="sm">
+			<Typography variant="h4" component="h1" align="center">
+				{'Login'}
+			</Typography>
+			<form noValidate>
+				<Grid container spacing={2}>
+					<Grid item xs={12}>
+						<TextField
+							variant="outlined"
+							label="Username"
+							fullWidth
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<PersonIcon />
+									</InputAdornment>
+								),
+							}}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							variant="outlined"
+							label="Password"
+							type="password"
+							fullWidth
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<PasswordIcon />
+									</InputAdornment>
+								),
+							}}
+						/>
+						<Typography variant="caption">
+							<Link to="">Forgot Password</Link>
+						</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Button variant="contained" color="primary" fullWidth>
+							Login
+						</Button>
+					</Grid>
+				</Grid>
+				<Grid container justify="flex-end">
+					<Grid item>
+						<Link to="/signup" variant="body2">
+							{'Need an account? Sign up'}
+						</Link>
+					</Grid>
+				</Grid>
+			</form>
+		</Container>
 	);
 };
 
-export default SignUp;
+export default Login;
