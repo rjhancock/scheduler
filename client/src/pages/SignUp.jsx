8c18778709157781c596df232345452e18cc5ae3
medium0fry <medium0fry@gmail.com>
2021-06-12 20:40:43 -0400

    Added icons to text fields

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -5,19 +5,28 @@ import {
 	Button,
 	Checkbox,
 	Container,
+	FormControl,
 	FormControlLabel,
 	Grid,
+	InputAdornment,
+	Radio,
+	RadioGroup,
 	TextField,
 	Typography,
 } from '@material-ui/core';
+
+import PersonIcon from '@material-ui/icons/PersonOutlineOutlined';
+import PasswordIcon from '@material-ui/icons/LockOutlined';
+import LockIcon from '@material-ui/icons/Lock';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
 
 const Copyright = () => {
 	return (
 		<Typography variant="body2" align="center">
 			{'Copyright '}
 			&copy;{' '}
-			<Link color="inherit" href="#">
-				FoxFry
+			<Link color="inherit" to="">
+				{'FoxFry'}
 			</Link>{' '}
 			{new Date().getFullYear()}
 			{'.'}
@@ -28,18 +37,39 @@ const Copyright = () => {
 const SignUp = () => {
 	return (
 		<Container component="main" maxWidth="sm">
-			<h1>Sign Up Page</h1>
+			<Typography variant="h4" component="h1" align="center">
+				{'Sign Up Page'}
+			</Typography>
 			<form noValidate>
 				<Grid container spacing={2}>
 					<Grid item xs={12}>
-						<TextField variant="outlined" label="Username" fullWidth />
+						<TextField
+							variant="outlined"
+							label="Username"
+							fullWidth
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<PersonIcon />
+									</InputAdornment>
+								),
+							}}
+						/>
 					</Grid>
 					<Grid item xs={12}>
 						<TextField
 							variant="outlined"
 							label="Email"
 							type="email"
+							autoCapitalize="email"
 							fullWidth
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<MailOutlineIcon />
+									</InputAdornment>
+								),
+							}}
 						/>
 					</Grid>
 					<Grid item xs={12}>
@@ -48,6 +78,13 @@ const SignUp = () => {
 							label="Password"
 							type="password"
 							fullWidth
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<PasswordIcon />
+									</InputAdornment>
+								),
+							}}
 						/>
 					</Grid>
 					<Grid item xs={12}>
@@ -56,16 +93,49 @@ const SignUp = () => {
 							label="Confirm Password"
 							type="password"
 							fullWidth
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<LockIcon />
+									</InputAdornment>
+								),
+							}}
 						/>
+					</Grid>
+					<Grid item xs={12}>
+						<FormControl component="fieldset">
+							<Typography component="legend">I am a(n)</Typography>
+							<RadioGroup>
+								<FormControlLabel
+									value="artist"
+									label="Artist"
+									control={<Radio />}
+								/>
+								<FormControlLabel
+									value="patron"
+									label="Patron"
+									control={<Radio />}
+								/>
+							</RadioGroup>
+						</FormControl>
 					</Grid>
 					<Grid item xs={12}>
 						<FormControlLabel
 							control={<Checkbox />}
 							label={
-								<span>
-									I agree to the{' '}
-									<Link to="">Terms and Conditions</Link>.
-								</span>
+								<Typography>
+									{'I have read and agree to the '}
+									<Link to="">{'Terms and Conditions'}</Link>.
+								</Typography>
+							}
+						/>
+						<FormControlLabel
+							control={<Checkbox />}
+							label={
+								<Typography>
+									{'I have read and agree to the '}
+									<Link to="">{'Privacy Policy'}</Link>.
+								</Typography>
 							}
 						/>
 					</Grid>
@@ -78,7 +148,7 @@ const SignUp = () => {
 				<Grid container justify="flex-end">
 					<Grid item>
 						<Link to="/login" variant="body2">
-							Already have an account? Log in
+							{'Already have an account? Log in'}
 						</Link>
 					</Grid>
 				</Grid>
