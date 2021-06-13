f97d0d0015d95e05c0e48d161c482d8aa66f4937
medium0fry <medium0fry@gmail.com>
2021-06-13 19:47:03 -0400

    Updated Gallery skeleton

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 import {
+	Box,
+	Button,
 	Container,
 	GridList,
 	GridListTile,
@@ -8,27 +13,65 @@ import {
 
 import InfoIcon from '@material-ui/icons/Info';
 
+import Navigation from '../components/Navigation';
+
 const Gallery = () => {
-	const tiles = [...Array(30)];
+	const { creator } = useParams();
+
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [tiles, setTiles] = useState([]);
+
+	useEffect(() => {
+		fetch('https://picsum.photos/v2/list')
+			.then((res) => res.json())
+			.then(
+				(data) => {
+					setIsLoaded(true);
+					setTiles(data);
+				},
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			);
+	}, []);
+
+	if (error) return <div>Error: {error.message}</div>;
+	if (!isLoaded) return <div>Loading...</div>;
 	return (
-		<Container>
-			<GridList cellHeight={180}>
-				{tiles.map((_, index) => (
-					<GridListTile key={index}>
-						<img src="https://picsum.photos/200/300" alt="" />
-						<GridListTileBar
-							title={'Test'}
-							subtitle={<span>by: {'Test'}</span>}
-							actionIcon={
-								<IconButton>
-									<InfoIcon color="secondary" />
-								</IconButton>
-							}
-						></GridListTileBar>
-					</GridListTile>
-				))}
-			</GridList>
-		</Container>
+		<div>
+			<Navigation />
+			<Container maxWidth="xl">
+				<Box textAlign="center">
+					<Button href={`/${creator}`} variant="contained" color="primary">
+						Request a Commission
+					</Button>
+				</Box>
+				<GridList cellHeight={225} cols={3}>
+					{tiles.map((tile, index) => (
+						<GridListTile
+							key={index}
+							style={{
+								marginTop: '8px',
+								padding: '4px',
+							}}
+						>
+							<img src={tile.download_url} alt="" />
+							<GridListTileBar
+								title={'Test'}
+								subtitle={<span>by: {tile.author}</span>}
+								actionIcon={
+									<IconButton>
+										<InfoIcon color="secondary" />
+									</IconButton>
+								}
+							></GridListTileBar>
+						</GridListTile>
+					))}
+				</GridList>
+			</Container>
+		</div>
 	);
 };
 
