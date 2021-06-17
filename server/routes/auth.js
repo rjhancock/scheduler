5b12f18c13d6539c64a1fd3c7f5190cb5cd4d4c7
medium0fry <medium0fry@gmail.com>
2021-06-16 21:28:28 -0400

    Added bcrypt as a dependency. Secured passwords by only storing hashes. Added Login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+
 import User from '../models/User.js';
 
 const router = express.Router();
@@ -6,10 +8,13 @@ const router = express.Router();
 //REGISTER
 router.post('/signup', async (req, res) => {
 	try {
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(req.body.password, salt);
+
 		const newUser = new User({
 			username: req.body.username,
 			email: req.body.email,
-			password: req.body.password,
+			password: hash,
 			role: req.body.role,
 		});
 
@@ -22,5 +27,20 @@ router.post('/signup', async (req, res) => {
 });
 
 //LOGIN
+router.post('/login', async (req, res) => {
+	try {
+		const user = User.findOne({ username: req.body.username });
+		!user && res.status(400).json('Wrong credentials!');
+
+		const validated = await bcrypt.compare(req.body.password, user.password);
+		!validated && res.status(400).json('Wrong credentials!');
+
+		// Prevent sending password
+		const { password, ...others } = user._doc;
+		res.status(200).json(others);
+	} catch (error) {
+		res.status(500).json(err);
+	}
+});
 
 export default router;
