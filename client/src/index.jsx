487792d9ef9e842abda1625c5a1cc8c3f3425042
medium0fry <medium0fry@gmail.com>
2021-07-26 14:26:19 -0400

    Setup handling subdomain routes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,18 +3,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Components
-import App from './components/App';
+import MainDomainViews from './pages/MainDomainViews';
+import SubDomainViews from './pages/SubDomainViews';
 import { Provider } from './context/Context';
 
 // Stylesheets
 import '@atlaskit/css-reset';
 import './index.css';
 
+const selectViews = () => {
+	const parsedData = window.location.host.split('.');
+	// If path has a subdomain, then go to that artists page
+	if (parsedData.length >= 3) {
+		return <SubDomainViews subdomain={parsedData[0]} />;
+	}
+	return <MainDomainViews />;
+};
+
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider>
-			<App />
-		</Provider>
+		<Provider>{selectViews()}</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
